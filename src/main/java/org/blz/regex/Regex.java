f70e92e196a354823ede20bf19f8fe277bd89dd2
sandeepvarma332 <ksandeepvarma99@gmail.com>
2021-10-20 00:41:43 +0530

    Added:firstname

--- src/main/java/org/blz/regex/Regex.java
+++ src/main/java/org/blz/regex/Regex.java
@@ -1,11 +1,28 @@
 package org.blz.regex;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
 public class Regex
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) {
+        Regex regex = new Regex();
+        regex.firstNameInput();
+    }
 
-    {
-        System.out.println( "Hello World!" );
+    public void firstNameInput() {
+
+        String firstNameInput = "Sandeep";
+        String regex = "^[[a-z]{1}A-Z]{1,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstNameInput);
+        if (matcher.matches()) {
+            System.out.println("\n VALID  Name ");
+        } else {
+            System.out.println("\n invalid Name");
+        }
     }
 }
